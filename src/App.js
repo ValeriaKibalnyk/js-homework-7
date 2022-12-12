98756c10ffc2e2e0bb1a4bb41f9eab7aa98755d5
ValeriaKibalnyk <valkibalnik@ukr.net>
2022-12-12 21:53:08 +0200

    first draft

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Currencies from './components/Currencies'; 
+
+function App() {
+  return <div className="container bg-white">
+    <h1>Currencies</h1>
+    <Currencies />
+  </div>
+}
+
+export default App;
+
